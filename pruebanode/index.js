409016c0b4601dcddf2376a383cc41630960cb76
jmacboy <j.m.alvarez.camacho@gmail.com>
2024-08-12 20:11:02 -0400

    agregado a clase del 2024-08-12

--- pruebanode/index.js
+++ pruebanode/index.js
@@ -1,8 +1,34 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const app = express()
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.set('view engine', 'ejs');
+
+
 
 app.get('/', function (req, res) {
-    res.send('Hello World')
+    // eslint-disable-next-line no-undef
+    res.sendFile(__dirname + '/views/index.html')
 })
+app.get('/holamundo', function (req, res) {
+    res.send('Hola Mundo')
+})
+app.post('/respuesta', function (req, res) {
+    console.log('Su nombre es: ' + req.body.nombre)
+    res.send('Su nombre es: ' + req.body.nombre + ' ' + req.body.apellido)
+})
+app.get('/respuesta', function (req, res) {
+    const nombre = req.query.nombre;
+    const apellido = req.query.apellido;
+    res.send('Su nombre es: ' + nombre + ' ' + apellido)
+})
+app.get('/respuestatexto', function (req, res) {
+    const nombre = req.query.nombre;
+    const apellido = req.query.apellido;
+    res.render('respuesta.ejs', { nombre: nombre, apellido: apellido });
+});
 
-app.listen(3000)
+app.listen(3000, function () {
+    console.log('Ingrese a http://localhost:3000')
+})
