0049ac24a930f5ffa23259935b9bb4d8dc1fc742
jmacboy <j.m.alvarez.camacho@gmail.com>
2024-09-16 19:12:17 -0400

    persona controller casi completo

--- nodeapi/controllers/persona.controller.js
+++ nodeapi/controllers/persona.controller.js
@@ -1,9 +1,29 @@
 const db = require("../models")
-
+// Estados del servidor
+//200 -> ok
+//201 -> creado
+//400 -> validaciones
+//401 -> no autorizado
+//403 -> prohibido
+//404 -> no encontrado
+//500 -> errores del servidor
 exports.listPersona = async (req, res) => {
     const personas = await db.personas.findAll();
     res.json(personas);
 }
+
+exports.getPersonaById = async (req, res) => {
+    const id = req.params.id;
+    const persona = await db.personas.findByPk(id);
+    if (!persona) {
+        res.status(404).json({
+            msg: 'Persona no encontrada'
+        });
+        return;
+    }
+    res.json(persona);
+}
+
 exports.createPersona = async (req, res) => {
     const persona = {
         nombre: req.body.nombre,
@@ -15,5 +35,42 @@ exports.createPersona = async (req, res) => {
         usuarioId: req.body.usuarioId
     }
     const personaCreada = await db.personas.create(persona);
-    res.json(personaCreada);
+    res.status(201).json(personaCreada);
+}
+exports.updatePersona = async (req, res) => {
+    const id = req.params.id;
+    const persona = await db.personas.findByPk(id);
+    if (!persona) {
+        res.status(404).json({
+            msg: 'Persona no encontrada'
+        });
+        return;
+    }
+
+    persona.nombre = req.body.nombre;
+    persona.apellido = req.body.apellido;
+    persona.edad = req.body.edad;
+    persona.ciudad = req.body.ciudad;
+    persona.fechaNacimiento = req.body.fechaNacimiento;
+    persona.genero = req.body.genero;
+    persona.usuarioId = req.body.usuarioId;
+
+    await db.personas.update(persona, {
+        where: { id: id }
+    });
+    res.json(persona);
+}
+exports.deletePersona = async (req, res) => {
+    const id = req.params.id;
+    const persona = await db.personas.findByPk(id);
+    if (!persona) {
+        res.status(404).json({
+            msg: 'Persona no encontrada'
+        });
+        return;
+    }
+    await persona.destroy();
+    res.json({
+        msg: 'Persona eliminada correctamente'
+    });
 }
--- nodeapi/routes/persona.routes.js
+++ nodeapi/routes/persona.routes.js
@@ -4,7 +4,12 @@ module.exports = app => {
         require("../controllers/persona.controller.js");
 
     router.get('/', controller.listPersona);
+    router.get('/:id', controller.getPersonaById);
     router.post('/', controller.createPersona);
+    router.put('/:id', controller.updatePersona);
+    //TODO: Hacer patch
+    // router.patch('/:id', controller.updatePersona);
+    router.delete('/:id', controller.deletePersona);
     app.use('/personas', router);
 
 };
